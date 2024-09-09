339b49b5ea0fa6f9a755e603d955d4f78195e3b8
JunePoon <bukharney@gmail.com>
2024-09-09 19:51:44 +0700

    fixed: req not show

--- frontend/src/components/chat/chat-layout.tsx
+++ frontend/src/components/chat/chat-layout.tsx
@@ -8,6 +8,8 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import LogoutPage from "../sidebarFooter";
+import { Button } from "../ui/button";
+import Footer from "../sidebarFooter";
 
 interface ChatLayoutProps {
   defaultLayout: number[] | undefined;
@@ -15,12 +17,57 @@ interface ChatLayoutProps {
   navCollapsedSize: number;
 }
 
+type FriendRequest = {
+  id: number;
+  username: string;
+};
+
 export function ChatLayout({
   defaultLayout = [],
   navCollapsedSize,
 }: ChatLayoutProps) {
-  const [users, setUsers] = useState<Friend[] | null>(null);
+  const [friendList, setFriendList] = useState<Friend[]>([]);
   const [selectedUser, setSelectedUser] = useState<Friend | null>(null);
+  const [friendRequestList, setFriendRequestList] = useState<FriendRequest[]>(
+    []
+  );
+
+  const handleAcceptFriend = async (username: string) => {
+    await fetch("/v1/users/add-friend", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({ username: username }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  const handleGetFriendRequests = async () => {
+    await fetch("/v1/users/friends-request", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data) {
+          setFriendRequestList([]);
+        }
+        setFriendRequestList(data);
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    handleGetFriendRequests();
+  }, []);
 
   useEffect(() => {
     const handleGetFriend = async () => {
@@ -32,7 +79,7 @@ export function ChatLayout({
         },
       })
         .then((res) => res.json())
-        .then((data) => setUsers(data));
+        .then((data) => setFriendList(data));
     };
 
     handleGetFriend();
@@ -52,8 +99,8 @@ export function ChatLayout({
           return data;
         });
 
-      if (res && users) {
-        const user = users.find((user) => user.id === userId);
+      if (res && friendList) {
+        const user = friendList.find((user) => user.id === userId);
         if (user) {
           user.messages = res;
           setSelectedUser(user);
@@ -64,7 +111,7 @@ export function ChatLayout({
     if (selectedUser) {
       handleGetMessages(selectedUser.room_id, selectedUser.id);
     }
-  }, [selectedUser, users]);
+  }, [selectedUser, friendList]);
 
   return (
     <ResizablePanelGroup
@@ -81,10 +128,10 @@ export function ChatLayout({
         collapsedSize={navCollapsedSize}
         minSize={30}
       >
-        {users &&
-          (users.length > 0 ? (
+        {friendList.length > 0 ? (
+          <div className="flex flex-col h-full justify-between">
             <Sidebar
-              links={users.map((user) => ({
+              links={friendList.map((user) => ({
                 id: user.id,
                 username: user.username,
                 messages: user.messages ?? [],
@@ -93,23 +140,112 @@ export function ChatLayout({
               setSelectedUser={setSelectedUser}
               selectedRoom={selectedUser}
             />
-          ) : (
-            <>
+            <PendingReq
+              friendRequestList={friendRequestList}
+              handleAcceptFriend={handleAcceptFriend}
+              handleGetFriendRequests={handleGetFriendRequests}
+            />
+            <Footer />
+          </div>
+        ) : (
+          <div className="flex flex-col h-full justify-between">
+            <PendingReq
+              friendRequestList={friendRequestList}
+              handleAcceptFriend={handleAcceptFriend}
+              handleGetFriendRequests={handleGetFriendRequests}
+            />
+            <div className="flex flex-col h-full justify-between">
               <div className="flex flex-col items-center justify-center h-full">
                 <p className="text-center text-lg font-medium text-zinc-500">
                   You have no friends yet
                 </p>
               </div>
               <LogoutPage />
-            </>
-          ))}
+            </div>
+          </div>
+        )}
       </ResizablePanel>
       <ResizableHandle withHandle />
       <ResizablePanel defaultSize={defaultLayout[1]} minSize={70}>
-        {selectedUser && (
+        {selectedUser ? (
           <Chat messages={selectedUser.messages} selectedUser={selectedUser} />
+        ) : (
+          <div className="flex flex-col h-full justify-between">
+            <div className="flex flex-col items-center justify-center h-full">
+              <p className="text-center text-lg font-medium text-zinc-500">
+                Select a friend to chat with
+              </p>
+            </div>
+          </div>
         )}
       </ResizablePanel>
     </ResizablePanelGroup>
+  );
+}
+
+function PendingReq({
+  friendRequestList,
+  handleAcceptFriend,
+  handleGetFriendRequests,
+}: {
+  friendRequestList: FriendRequest[];
+  handleAcceptFriend: (username: string) => void;
+  handleGetFriendRequests: () => void;
+}) {
+  return (
+    <div className="flex flex-col h-6/12 p-2 overflow-auto">
+      <div className="flex justify-between p-2 items-centerbg-slate-300">
+        <div className="flex gap-2 items-center text-2xl">
+          <p className="font-medium">Pending requests</p>
+        </div>
+      </div>
+
+      <div className="grid gap-1 px-2 overflow-auto h-48 items-start ">
+        {!friendRequestList ? (
+          <div className="flex justify-center items-center h-full">
+            <p className="text-center font-medium text-zinc-500">
+              No pending requests
+            </p>
+          </div>
+        ) : (
+          <>
+            {friendRequestList &&
+              friendRequestList.map((link, index) => (
+                <div
+                  className="flex justify-between p-2 items-center "
+                  key={index}
+                >
+                  <div className="flex flex-col max-w-28">
+                    <span>{link.username}</span>
+                  </div>
+                  <div className="flex gap-2">
+                    <Button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        console.log("Accepting friend request");
+                      }}
+                      variant={"outline"}
+                      size={"sm"}
+                    >
+                      Reject
+                    </Button>
+                    <Button
+                      size={"sm"}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleAcceptFriend(link.username);
+                        handleGetFriendRequests();
+                        console.log("Accepting friend request");
+                      }}
+                    >
+                      Accept
+                    </Button>
+                  </div>
+                </div>
+              ))}
+          </>
+        )}
+      </div>
+    </div>
   );
 }
--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
 import { Friend, Message } from "../data";
-import Footer from "./sidebarFooter";
 import React from "react";
-import { Button } from "./ui/button";
 
 interface SidebarProps {
   links: {
@@ -15,55 +12,11 @@ interface SidebarProps {
   setSelectedUser: React.Dispatch<React.SetStateAction<Friend | null>>;
 }
 
-type FriendRequest = {
-  id: number;
-  username: string;
-};
 export function Sidebar({
   links,
   setSelectedUser,
   selectedRoom,
 }: SidebarProps) {
-  const [friendRequestList, setFriendRequestList] = useState<FriendRequest[]>(
-    []
-  );
-  const handleGetFriendRequests = async () => {
-    await fetch("/v1/users/friends-request", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data) {
-          setFriendRequestList([]);
-        }
-        setFriendRequestList(data);
-        console.log(data);
-      });
-  };
-
-  const handleAcceptFriend = async (username: string) => {
-    await fetch("/v1/users/add-friend", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-      body: JSON.stringify({ username: username }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
-  };
-
-  useEffect(() => {
-    handleGetFriendRequests();
-  }, []);
-
   return (
     <div className="flex flex-col h-full justify-between">
       <div className="flex flex-col h-6/12 gap-4 p-2">
@@ -73,7 +26,6 @@ export function Sidebar({
             <span className="text-zinc-300">({links.length})</span>
           </div>
         </div>
-
         {links.length > 0 && (
           <nav className="grid gap-1 px-2">
             {links.map((link, index) => (
@@ -96,64 +48,6 @@ export function Sidebar({
           </nav>
         )}
       </div>
-      <div className="flex flex-col h-6/12 p-2 overflow-auto">
-        <div className="flex justify-between p-2 items-centerbg-slate-300">
-          <div className="flex gap-2 items-center text-2xl">
-            <p className="font-medium">Pending requests</p>
-            <span className="text-zinc-300">
-              ({(friendRequestList && friendRequestList.length) || 0})
-            </span>
-          </div>
-        </div>
-
-        <div className="grid gap-1 px-2 overflow-auto h-48 items-start ">
-          {!friendRequestList ? (
-            <div className="flex justify-center items-center h-full">
-              <p className="text-center font-medium text-zinc-500">
-                No pending requests
-              </p>
-            </div>
-          ) : (
-            <>
-              {friendRequestList &&
-                friendRequestList.map((link, index) => (
-                  <div
-                    className="flex justify-between p-2 items-center "
-                    key={index}
-                  >
-                    <div className="flex flex-col max-w-28">
-                      <span>{link.username}</span>
-                    </div>
-                    <div className="flex gap-2">
-                      <Button
-                        onClick={(e) => {
-                          e.preventDefault();
-                          console.log("Accepting friend request");
-                        }}
-                        variant={"outline"}
-                        size={"sm"}
-                      >
-                        Reject
-                      </Button>
-                      <Button
-                        size={"sm"}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          handleAcceptFriend(link.username);
-                          handleGetFriendRequests();
-                          console.log("Accepting friend request");
-                        }}
-                      >
-                        Accept
-                      </Button>
-                    </div>
-                  </div>
-                ))}
-            </>
-          )}
-        </div>
-      </div>
-      <Footer />
     </div>
   );
 }
